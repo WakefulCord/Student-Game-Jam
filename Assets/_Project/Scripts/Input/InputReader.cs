cc89fcc46622b47445cb870aa28b2d9c8fa85315
Tom Cole <tom.cole04@outlook.com>
2025-10-22 23:10:29 +0100

    Merged on return of the WiFi

--- Assets/_Project/Scripts/Input/InputReader.cs
+++ Assets/_Project/Scripts/Input/InputReader.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class InputReader : MonoBehaviour
+namespace Student-Game-Jam
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public class InputReader : ScriptableObject, PlayerInputActions.IPlayerActions
     {
-        
+    public event UnityAction<Vector2> Move = delegate { };
+    public event UnityAction<Vector2, bool> Look = delegate { };
+    public event UnityAction EnableMouseControlCamera = delegate { };
+    public event UnityAction DisableMouseControlCamera = delegate { };
+
+    PlayerInputActions inputActions;
+
+    public Vector3 Direction => inputActions.Player.Move.ReadValue<Vector2>();
+
+    void OnEnable()
+    {
+        if(inputActions == null)
+        {
+            inputActions = new PlayerInputActions();
+            inputActions.Player.SetCallbacks(this);
+        }
+        inputActions.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnMove(InputAction.CallbackContext context)
+        {
+        Move.Invoke(context.ReadValue<Vector2>());
+        }
+
+    public void OnLook(InputAction.CallbackContext context)
+        {
+        Look.Invoke(context.ReadValue<Vector2>(), IsDeviceMouse(context));
     }
+
+    bool IsDeviceMouse(InputAction.CallbackContext context) => context.control.device.name == "Mouse";
+    
+    public void OnFire(InputAction.CallbackContext context)
+        {
+            //noop
+        }
+
+    public void OnMouseControlCamera(InputAction.CallbackContext context)
+        {
+            //noop
+        }
+
+    public void OnRun(InputAction.CallbackContext context)
+        {
+            //noop
+        }
+
+    public void OnJump(InputAction.CallbackContext context)
+        {
+            //noop
+        }
+
+    }  
+
 }
